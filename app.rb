47dd5e885644bca3095d26b2edac4d6d6ff24f5d
huro3h <huro3h@gmail.com>
2020-09-21 02:42:24 +0900

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,3 +1,28 @@
+require 'slack-ruby-client'
+
+DAY = 86_400
+WEEK = DAY * 7
+PREVIOUS_WEEK = Time.now - WEEK
+
+APP_TOKEN = ENV['APP_TOKEN']
+CHANNEL_ID = ENV['CHANNEL_ID']
+
 def lambda_handler(event:, context:)
-  puts "hoge"
+  Slack.configure do |config|
+    config.token = APP_TOKEN
+  end
+
+  client = Slack::Web::Client.new
+  messages = client.conversations_history(channel: CHANNEL_ID, count: 100)['messages']
+  target_messages = outside_messages(messages)
+
+  target_messages.each do |target_message|
+    client.chat_delete(channel: CHANNEL_ID, ts: target_message.ts)
+    sleep(Random.rand(5))
+  end
+end
+
+# return [Array] メッセージの配列から1週間以上前のメッセージを返す(削除対象)
+def outside_messages(messages)
+  messages.select{ |message| message.ts < PREVIOUS_WEEK.to_s }
 end
